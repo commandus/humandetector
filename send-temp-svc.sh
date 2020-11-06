dd6245871b514c0a52424e40394600ac51f1e8e8
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2020-11-06 13:16:39 +0900

    doc

--- send-temp-svc.sh
+++ send-temp-svc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec 6<&0
-echo "--a 1" | ./usb-rfid-card -r -k 6
+./human-detector /dev/ttyACM1 | ./usb-rfid-card -k 6 | ./put-temperature-db | ./put-temperature-json | jq .
 exec 0<&6 6<&-
 exit 0
